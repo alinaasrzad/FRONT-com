555caacfc5d0f332c79afa88705b0c532004f2a8
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-11-10 09:47:02 +0330

    added package

---
+++ src/COMPONENTS/BODY/Package.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Package = () => {
+  return (
+   
+    <div className="flex justify-center p-60 space-x-3" >
+        <div> </div>
+      <div className="bg-white w-24 h-28 rounded-lg ">
+        <div className="w-14 mr-5 my-2 ">
+          <img className=" hover:opacity-30" src="https://azarland.com/wp-content/uploads/2022/08/66da033f8ae5dde5c879ad91dc0e830d2941aebe_1642801994.png" alt="" />
+          <p className="text-sm mr-3 my-3" >آذرپی</p>
+        </div>
+      </div>
+       <div className="bg-white w-24 h-28 rounded-lg ">
+        <div className="w-14 mr-5 my-2 ">
+          <img className="hover:opacity-20" src="https://azarland.com/wp-content/uploads/2022/08/d0dc31c892be8cf1408e4e14580b3f479da66bd1_1648897133.png" alt="" />
+          <p className="text-sm mr-3 my-3"  >آذرپی</p>
+        </div>
+      </div>
+      <div className="bg-white w-24 h-28 rounded-lg ">
+        <div className="w-14 mr-5 my-2 ">
+          <img className="hover:opacity-30" src="https://azarland.com/wp-content/uploads/2022/08/ac127167132653d14c758748b07824a6a7643a31_1648897095.png" alt="" />
+          <p className="text-sm mr-3 my-3"  >آذرپی</p>
+        </div>
+      </div>
+      <div className="bg-white w-24 h-28 rounded-lg ">
+        <div className="w-14 mr-5 my-2 ">
+          <img className=" hover:opacity-30" src="https://azarland.com/wp-content/uploads/2022/08/17bb6daa07ae2ec11867fb7320ed6f79b26f1f4b_1648897081.png" alt="" />
+          <p className="text-sm mr-3 my-3"  >آذرپی</p>
+        </div>
+      </div>
+      <div className="bg-white w-24 h-28 rounded-lg ">
+        <div className="w-14 mr-5 my-2">
+          <img className=" hover:opacity-30" src="https://azarland.com/wp-content/uploads/2022/08/33f94db0e93a29b08b5c45d7932dbb114791155d_1658329987.png" alt="" />
+          <p className="text-sm mr-3 my-3"  >آذرپی</p>
+        </div>
+      </div>
+      <div className="bg-white w-24 h-28 rounded-lg  ">
+        <div className="w-14 mr-5 my-2">
+          <img className=" hover:opacity-30" src="https://azarland.com/wp-content/uploads/2022/08/967cbaa25713ca4d0e58cb5aaf0e486ab986d460_1648898393.png" alt="" />
+          <p className="text-sm mr-3 my-3"  >آذرپی</p>
+        </div>
+      </div>
+      <div className="bg-white w-24 h-28 rounded-lg   ">
+        <div className="w-14 mr-5 my-2">
+          <img className=" hover:opacity-30" src="https://azarland.com/wp-content/uploads/2022/08/78ccd40cbf305fb067de78ddab5be84f69589c8d_1648897009.png" alt="" />
+          <p className="text-sm mr-3 my-3"  >آذرپی</p>
+        </div>
+      </div>
+    </div>
+   
+  );
+};
+
+export default Package;
