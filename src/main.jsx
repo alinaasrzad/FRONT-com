d1a94c31d97f4f85c298a54dbecf905b28ca2fd2
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-11-10 10:26:26 +0330

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header/>
-   <Body/>
-    <Footer/>
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Headers from "./components/header/Headers";
+import Footer from "./components/footer/Footer";
+import "@fortawesome/fontawesome-free/css/all.css";
+import Body from "./COMPONENTS/BODY/Body.jsx";
+ 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Headers />
+  <Body />
+    <Footer />
+  </React.StrictMode>
+);
